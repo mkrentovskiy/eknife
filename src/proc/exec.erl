b91443f84404e2bc21bbe454a1daf24568a3db28
Max Krentovskiy <mkrentovskiy@gmail.com>
2023-01-16 19:36:49 +0300

    Ads sync execution proc. Add tranlator for integers to the russian words.

--- src/proc/exec.erl
+++ src/proc/exec.erl
@@ -1,8 +1,41 @@
 -module(exec).
 
--export([drop_port/1]).
+-export([sync/2, sync/3, drop_port/1]).
 
 -include("eknife.hrl").
+
+
+-spec sync(list() | binary(), pid() | undefined) -> ok | {error, integer()} | {error, no_port}.
+
+sync(Cmd, TopProc) ->
+    sync(Cmd, TopProc, []).
+
+-spec sync(list() | binary(), pid() | undefined, list()) -> ok | {error, integer()} | {error, no_port}.
+
+sync(Cmd, TopProc, MoreOptions) ->
+    case erlang:open_port({spawn, cast:to_list(Cmd)}, [exit_status] ++ MoreOptions) of
+        Port when is_port(Port) ->
+            sync_loop(Port, TopProc);
+        Error ->
+            ?LOG_ERROR("Can't start port for command ~p - ~p", [Cmd, Error]),
+            {error, no_port}
+    end.
+
+sync_loop(Port, TopProc) ->          
+  receive
+    {Port, {exit_status, 0}} ->
+      ok;
+    {Port, {exit_status, Status}} ->
+      {error, Status};
+    Message when is_pid(TopProc) ->
+        ?LOG_DEBUG("Forward ~p to ~p", [Message, TopProc]),
+        TopProc ! Message,
+        sync_loop(Port, TopProc);
+    Message ->
+        ?LOG_DEBUG("Igonre message ~p", [Message]),
+        sync_loop(Port, TopProc)
+  end.
+
 
 -spec drop_port(undefined | port()) -> ok | undefined.
 
